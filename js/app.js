b0a68a9475a2a16ff490eb52decb1e6bda316094
Brad Smith <bradsmith@duck.com>
2023-09-27 12:40:30 -0400

    Finish #3

--- js/app.js
+++ js/app.js
@@ -21,3 +21,17 @@ Exercise 2:
 foods.push('pizza');
 foods.push('cheeseburger');
 console.log('Exercise 2 Result:\n',  foods);
+
+/*
+Exercise 3:
+  - Add the string 'taco' to the foods array so that 'taco' is the first food in the array.
+*/
+
+// Complete Exercise 3 below...
+
+// Option 1: unshift()
+// foods.unshift('taco');
+
+// Option 2: splice() aka add at index 0 without overwriting anything
+foods.splice(0, 0, 'taco');
+console.log('Exercise 3 Result:\n', foods);
